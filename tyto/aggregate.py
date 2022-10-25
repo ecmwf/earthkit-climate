7d219212595112f2d82bdd8656e92cd7547e8071
Eddy <Edward.Comyn-Platt@ecmwf.int>
2022-10-25 16:35:36 +0100

    bigger tidy

--- tyto/aggregate.py
+++ tyto/aggregate.py
@@ -114,7 +114,7 @@ def groupby_time(
     frequency: str = None,
     bin_widths: int = None,
     squeeze: bool = True,
-    time_dim = 'time',
+    time_dim="time",
 ):
     if frequency is None:
         try:
@@ -128,7 +128,9 @@ def groupby_time(
         bin_widths = bin_widths or possible_bins
 
     if bin_widths is not None:
-        return _groupby_bins(dataarray, frequency, bin_widths, squeeze, time_dim=time_dim)
+        return _groupby_bins(
+            dataarray, frequency, bin_widths, squeeze, time_dim=time_dim
+        )
 
     try:
         grouped_data = dataarray.groupby(f"{time_dim}.{frequency}", squeeze=squeeze)
@@ -145,7 +147,7 @@ def _groupby_bins(
     frequency: str,
     bin_widths: int,
     squeeze: bool,
-    time_dim: str = 'time',
+    time_dim: str = "time",
 ):
     if not isinstance(bin_widths, (list, tuple)):
         max_value = _BIN_MAXES[frequency]
@@ -171,12 +173,8 @@ def _pandas_frequency_and_bins(
     return freq, bins
 
 
-
 def rolling_reduce(
-        dataarray: xr.DataArray,
-        how_reduce='mean',
-        how_dropna='any',
-        **kwargs
+    dataarray: xr.DataArray, how_reduce="mean", how_dropna="any", **kwargs
 ) -> xr.DataArray:
     """Return reduced data using a moving window over which to apply the reduction.
 
@@ -198,7 +196,7 @@ def rolling_reduce(
     how_dropna (optional): str
         Determine if dimension is removed from the output when we have at least one NaN or
         all NaN. **how_dropna** can be 'None', 'any' or 'all'. Default is 'any'.
-    **kwargs : 
+    **kwargs :
         Any kwargs that are compatible with the select `how_reduce` method.
 
     Returns
@@ -206,19 +204,13 @@ def rolling_reduce(
     xr.DataArray
     """
     # Expand dim kwarg to individual kwargs
-    if isinstance(kwargs.get('dim'), dict):
-        kwargs.update(kwargs.pop('dim'))
+    if isinstance(kwargs.get("dim"), dict):
+        kwargs.update(kwargs.pop("dim"))
 
-    window_dims = [
-        _dim for _dim in list(dataarray.dims) if _dim in list(kwargs)
-    ]
-    rolling_kwargs_keys = ['min_periods', 'center'] + window_dims
-    rolling_kwargs_keys = [
-        _kwarg for _kwarg in kwargs if _kwarg in rolling_kwargs_keys
-    ]
-    rolling_kwargs = {
-        _kwarg: kwargs.pop(_kwarg) for _kwarg in rolling_kwargs_keys
-    }
+    window_dims = [_dim for _dim in list(dataarray.dims) if _dim in list(kwargs)]
+    rolling_kwargs_keys = ["min_periods", "center"] + window_dims
+    rolling_kwargs_keys = [_kwarg for _kwarg in kwargs if _kwarg in rolling_kwargs_keys]
+    rolling_kwargs = {_kwarg: kwargs.pop(_kwarg) for _kwarg in rolling_kwargs_keys}
 
     # Any kwargs left after above reductions are kwargs for reduction method
     reduce_kwargs = kwargs
@@ -227,17 +219,14 @@ def rolling_reduce(
     data_rolling = dataarray.rolling(**rolling_kwargs)
 
     in_built_how_methods = [
-        method for method in dir(data_rolling)
-        if not method.startswith('_')
+        method for method in dir(data_rolling) if not method.startswith("_")
     ]
     if how_reduce in in_built_how_methods:
         data_windowed = data_rolling.__getattribute__(how_reduce)(**reduce_kwargs)
-    else:   # Check for tyto HOW methods
-        data_windowed = data_rolling.reduce(
-            HOW_DICT[how_reduce], reduce_kwargs
-        )
+    else:  # Check for tyto HOW methods
+        data_windowed = data_rolling.reduce(HOW_DICT[how_reduce], reduce_kwargs)
 
-    if how_dropna not in [None, 'None', 'none']:
+    if how_dropna not in [None, "None", "none"]:
         for dim in window_dims:
             data_windowed = data_windowed.dropna(dim, how=how_dropna)
 
