548456643a4aeb6741d1688ffe65c29749ef00ab
Christopher Polster <christopher.polster@ecmwf.int>
2026-03-13 11:36:05 +0100

    Adopt earthkit-customised sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -10,11 +10,6 @@ import datetime
 import os
 import sys
 
-# This doesn't work, so need to find another way
-# from earthkit import climate
-# version = climate.__version__
-# release = climate.__version__
-
 sys.path.insert(0, os.path.abspath("./"))
 sys.path.insert(0, os.path.abspath("../"))
 
@@ -35,9 +30,11 @@ copyright = "%s, European Centre for Medium-Range Weather Forecasts (ECMWF)" % (
 # ones.
 extensions = [
     "autoapi.extension",
-    "nbsphinx",
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "nbsphinx",
 ]
 
 # autodoc configuration
@@ -54,11 +51,11 @@ autoapi_options = [
     "inherited-members",
     "undoc-members",
     "show-inheritance",
-    "show-module-summary",
+    # "show-module-summary",
     "imported-members",
 ]
 autoapi_root = "_api"
-autoapi_own_page_level = "function"
+# autoapi_own_page_level = "function"
 
 # napoleon configuration
 napoleon_google_docstring = False
@@ -79,10 +76,55 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
-html_logo = "_static/earthkit-climate-light.svg"
+html_theme = "furo"
+# html_logo = "_static/earthkit-climate-light.svg"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = [
+    "custom.css",
+]
+
+html_theme_options = {
+    "light_css_variables": {
+        "color-sidebar-background": "#131320",
+        # "color-background-primary": "", # leave as default to avoid overriding the light theme background
+        "color-sidebar-link-text": "#ffffff",
+        "color-sidebar-brand-text": "#ffffff",
+        "color-sidebar-caption-text": "#ffffff",
+        "color-brand-primary": "#FCE54B",
+        "color-brand-content": "#5f8dd3",
+        "color-sidebar-item-background--hover": "#001F3F",
+        "color-sidebar-item-expander-background--hover": "#001F3F",
+    },
+    "dark_css_variables": {
+        "color-sidebar-background": "#131320",
+        "color-background-primary": "#131320",
+        "color-sidebar-link-text": "#ffffff",
+        "color-sidebar-brand-text": "#ffffff",
+        "color-sidebar-caption-text": "#ffffff",
+        "color-brand-primary": "#FCE54B",
+        "color-brand-content": "#5f8dd3",
+        "color-sidebar-item-background--hover": "#001F3F",
+        "color-sidebar-item-expander-background--hover": "#001F3F",
+    },
+    "light_logo": "earthkit-climate-dark.svg",
+    "dark_logo": "earthkit-climate-dark.svg",
+    "source_repository": "https://github.com/ecmwf/earthkit-climate/",
+    # "source_branch": source_branch,
+    "source_directory": "docs/source",
+    "footer_icons": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/ecmwf/earthkit-climate",
+            "html": """
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                </svg>
+            """,
+            "class": "",
+        },
+    ],
+}
